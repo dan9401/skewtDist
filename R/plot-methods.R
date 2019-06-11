1595d3c928f2fbd00e9aaf4c7de01b450ed6311c
dan9401 <dansummer94@gmail.com>
2019-06-11 09:08:38 -0700

    rename plot functions

--- R/plot-methods.R
+++ R/plot-methods.R
@@ -3,10 +3,20 @@
 #' @description Plot methods for the AST fit class
 #' @param fit A AST fit object of class \code{\link{astfit}}
 #' @name plot-methods
+#' @examples
+#' data = rast(1000, 1.5, 2, 0.8, 3, 4)
+#' spec = astspec(data)
+#' solver_control = list("algorithm" = "NLOPT_LN_COBYLA", "maxeval" = 1.0e5, "xtol_rel" = 1.0e-8)
+#' fit = astfit(spec, "nloptr", solver_control)
+#'
+#' density_ast(fit)
+#' qqplot_ast(fit, dist = "normal")
+#' qqplot_ast(fit, dist = "t")
+#' qqplot_ast(fit, dist = "ast")
 
 #' @rdname plot-methods
 #' @export
-plot_density = function(fit) {
+density_ast = function(fit) {
   data = fit$data
   pars = fit$fitted_pars
   mu = pars[1]; sigma = pars[2]; alpha = pars[3]; nu1 = pars[4]; nu2 = pars[5]
@@ -21,21 +31,21 @@ plot_density = function(fit) {
 
 #' @rdname plot-methods
 #' @export
-qqast = function(fit, method = "normal") {
+qqplot_ast = function(fit, dist = "normal", ...) {
   y = fit$data
   pars = fit$fitted_pars
   mu = pars[1]; sigma = pars[2]; alpha = pars[3]; nu1 = pars[4]; nu2 = pars[5]
 
   p = past(y, mu, sigma, alpha, nu1, nu2)
-  if(method == "normal") {
+  if(dist == "normal") {
     x = qnorm(p, mean = mu, sd = sigma)
     px = qnorm(c(0.25, 0.75))
-  } else if (method == "t") {
+  } else if (dist == "t") {
     x = qt(p, df = max(nu1, nu2))
-  } else if (method == "ast") {
+  } else if (dist == "ast") {
     x = qast(p, mu, sigma, alpha, nu1, nu2)
   }
-  qqplot(x, y, xlim = c())
+  qqplot(x, y, ...)
 
   py = quantile(y, c(0.25, 0.75))
   slope = diff(y) / diff(x)
